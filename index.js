1721bd189ffcbad3457b85e291dca62afc722a67
Aaron Shannon <Aaron@wbir-aaron.local>
2016-10-07 16:49:01 -0400

    restructed for multiple designs, started basic weather alternative for design 1

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/html'));
-app.use(express.static(__dirname + '/js'));
-app.use(express.static(__dirname + '/css'));
-app.use(express.static(__dirname + '/images'));
+app.use('/01',express.static(__dirname + '/01'));
+app.use('/01-weather',express.static(__dirname + '/01-weather'));
+app.use('/02',express.static(__dirname + '/02'));
+app.use('/03',express.static(__dirname + '/03'));
 
 app.listen(3000, function () {
   console.log('Listening on port 3000...');
